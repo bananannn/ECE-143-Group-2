cbab0262238ee3925f8673cdc18e8b2eedfb5ddc
Moyan Zhou <32554995+bananannn@users.noreply.github.com>
2018-12-03 23:05:19 -0800

    traffic incidents(freeways) python file

---
+++ traffic_incidents_freeway.py
@@ -0,0 +1,112 @@
+import pandas as pd
+import heapq
+from bokeh.plotting import figure, output_file, show
+from bokeh.palettes import brewer
+
+
+def load_pems_incidents(traffic):
+    '''
+    The function load the PEMS one-month traffic incidents files and clean the data
+    :param traffic: str - the name of the dataset
+    :return: a formed dataset
+    '''
+    assert isinstance(traffic, str)
+
+    traffic = pd.read_csv(traffic, sep=',', header=None)
+    traffic.columns = ['Incident ID', 'CC code', 'Incident Number', 'Timestamp', 'Description',
+                       'Location', 'Area', 'Zoom Map', 'TB xy', 'lat', 'lon', 'District',
+                       'County FIPS ID', 'City FIPS ID', 'Freeway Number', 'Freeway Direction',
+                       'State Postmile', 'Absolute Postmile', 'Severity', 'Duration']
+
+    traffic = traffic[(traffic['lat'] <= 34.4992) & (traffic['lon'] <= -117.7831) & (traffic['lon'] >= -118.6917) & (
+                traffic['lat'] >= 33.7059)]
+
+    traffic['Timestamp'] = pd.to_datetime(traffic['Timestamp'])
+    traffic['hours'] = traffic['Timestamp'].dt.hour
+
+    return traffic
+
+
+def top_10_freeways(traffic):
+    '''
+    Return a list with top 10 freeways which has most incidents' numbers
+    :param traffic: the traffic dataframe
+    :return: a list with top 10 freeways' numbers
+    '''
+
+    assert isinstance(traffic, pd.DataFrame)
+
+    freeways = dict(traffic.groupby(['Freeway Number'])['Freeway Number'].count())
+    top_10_freeways_tuple = heapq.nlargest(10, freeways.items(), key=lambda i: i[1])
+
+    return [x[0] for x in top_10_freeways_tuple]
+
+
+def total_traffic_incidents_dayofmonth(traffic, fname):
+    '''
+    Input a traffic dataframe with extracted hour information
+    return a line plot of the total number of incidents happened in each of the hours
+
+    :param traffic: a cleaned dataframe
+           fname: output file name
+    :return: a line plot
+    '''
+
+    assert isinstance(traffic, pd.DataFrame)
+    assert isinstance(fname, str)
+
+    output_file(fname)
+
+    p = figure(title='Total Traffic incidents in one month', plot_width=400, plot_height=400)
+
+    incidents_number = list(dict(traffic.groupby('hours')['hours'].count()).values())
+    # add a line renderer
+    p.line(list(range(24)), incidents_number, line_width=2)
+
+    p.xaxis.axis_label = 'Hour of a day'
+    p.yaxis.axis_label = 'Number of traffic incidents'
+
+    show(p)
+
+
+def traffic_incidents_freeways_dayofmonth(traffic, fname):
+    '''
+    a plot with different freeways' incidents' number in different hours of a month
+    :param traffic: a cleaned dataframe
+    :param fname: output file name
+    :return: a line plot with many different lines
+    '''
+
+    assert isinstance(traffic, pd.DataFrame)
+    assert isinstance(fname, str)
+
+    incidents_oneday_all_10_ways = []
+
+    freeways = dict(traffic.groupby(['Freeway Number'])['Freeway Number'].count())
+    top_10_freeways_tuple = heapq.nlargest(10, freeways.items(), key=lambda i: i[1])
+    top_10_freeways = [x[0] for x in top_10_freeways_tuple]
+
+    for j in top_10_freeways:
+        incidents_oneday_oneway = []
+        for i in list(range(24)):
+            incidents_oneday_oneway.append(traffic[(traffic['hours'] == i) & (traffic['Freeway Number'] == j)].shape[0])
+
+        incidents_oneday_all_10_ways.append(incidents_oneday_oneway)
+
+    output_file(fname)
+
+    p = figure(title='Total Traffic incidents in one month', plot_width=950, plot_height=600)
+    colors_10 = brewer['Spectral'][10]
+
+    # add a line renderer
+    for i in range(10):
+        current_label = "Freeway Number: " + str(top_10_freeways[i])
+        p.line(list(range(24)), incidents_oneday_all_10_ways[i], color=colors_10[i], legend=current_label, line_width=2)
+
+    p.xaxis.axis_label = 'Month'
+    p.yaxis.axis_label = 'Number of traffic incidents'
+    p.legend.label_text_font_size = '8pt'
+
+    show(p)
+
+
